aa8f13ec50ef3ab333357ae246f1f9338de4a144
tayaracaires <tayaracaires@gmail.com>
2019-11-28 17:40:58 -0300

    criando a função mdLinks

--- cli.js
+++ cli.js
@@ -1,1 +1,9 @@
 #!/usr/bin/env node
+
+const mdLinks = require('./lib/index.js')
+
+mdLinks()
+    .then((result) => {
+        console.log(result))
+    }    
+    .catch((error) => console.log(error))
